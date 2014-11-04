59c083292ec36122d0ad3326f570acf19e8c7ca5
David Sancho <dsnxmoreno@gmail.com>
2014-11-04 18:28:11 +0100

    Added  and export example.json

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 (function() {
 
-
     var app = angular.module("nexo", []);
 
-    app.controller("tableController", function() {
+    app.controller("tableController", ['$http', function($http) {
 
-        this.table = json;
+        var table = this;
+
+        table.data = [];
+
+        $http.get('/example.json').success(function(data) {
+            table.data = data;
+        });
 
         this.addRound = function(band) {
             ++this.table.server[band].rounds;
@@ -15,86 +20,5 @@
             ++this.table.server[band].players[player_id].Stats[stat];
         };
 
-    });
-
-    var json  = {
-        "server": {
-
-            "type" : "Ranked Game",
-
-            "map" : "de_dust2",
-
-            "date" : new Date(),
-
-            "time" : "$ERVER_TIME()",
-            "url_spectate" : "url_spectate",
-
-            "T" : {
-                "name" : "Terrorist",
-                "rounds" : "4",
-                "players" : [
-                    {
-                        "id": 1,
-                        "Steam_Community" : "steamcommunity.com/asd",
-                        "Steam_Picture" : "http://placeholder.ws/png/25",
-                        "Steam_Name" : "Steam_Name",
-                        "SteamID" : "123456789",
-                        "ESL_profile" : "esl.eu/player/dios",
-                        "Range" : "Global Elite",
-                        "Medal" : "Operation Payback",
-                        "Hours" : "1000",
-                        "MatchesWon" : "888",
-                        "Stats" : { "kill" : "3", "death" : "4", "assist" : "5" }
-                    },
-                    {
-                        "id": 2,
-                        "Steam_Community" : "steamcommunity.com/asd",
-                        "Steam_Picture" : "http://placeholder.ws/png/25",
-                        "Steam_Name" : "Steam_Name",
-                        "SteamID" : "123456789",
-                        "ESL_profile" : "esl.eu/player/dios",
-                        "Range" : "Global Elite",
-                        "Medal" : "Operation Payback",
-                        "Hours" : "1000",
-                        "MatchesWon" : "888",
-                        "Stats" : { "kill" : "3", "death" : "4", "assist" : "5" }
-                    }  
-                ]
-            },
-
-             "CT" : { 
-                "name" : "Counter Terrorist",
-                "rounds" : "3",
-                "players" : [
-                    {
-                        "id": 1,
-                        "Steam_Community" : "steamcommunity.com/asd",
-                        "Steam_Picture" : "http://placeholder.ws/png/25",
-                        "Steam_Name" : "Steam_Name",
-                        "SteamID" : "123456789",
-                        "ESL_profile" : "esl.eu/player/dios",
-                        "Range" : "Global Elite",
-                        "Medal" : "Operation Payback",
-                        "Hours" : "1000",
-                        "MatchesWon" : "888",
-                        "Stats" : { "kill" : "3", "death" : "4", "assist" : "5" }
-                    },
-                    {
-                        "id": 2,
-                        "Steam_Community" : "steamcommunity.com/asd",
-                        "Steam_Picture" : "http://placeholder.ws/png/25",
-                        "Steam_Name" : "Steam_Name",
-                        "SteamID" : "123456789",
-                        "ESL_profile" : "esl.eu/player/dios",
-                        "Range" : "Global Elite",
-                        "Medal" : "Operation Payback",
-                        "Hours" : "1000",
-                        "MatchesWon" : "888",
-                        "Stats" : { "kill" : "3", "death" : "4", "assist" : "5" }
-                    }   
-                ]
-            }
-        }
-    }
-
+    }]);
 })();
